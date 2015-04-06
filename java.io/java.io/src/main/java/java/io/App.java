6045e7c930196c58d2a1263e2554e15ac5ef98da
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2015-04-06 20:01:00 +0100

    New example about io in java

---
+++ java.io/java.io/src/main/java/java/io/App.java
@@ -0,0 +1,20 @@
+package java.io;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	
+    	OutputStream out = null;
+    	PrintStream ps = new PrintStream(out);
+    
+    	
+    
+    }
+}
