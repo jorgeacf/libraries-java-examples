c623cc48e4cd810e87ddf98e3d754171c0585f43
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2015-04-21 21:04:02 +0000

    Added example of streams copy, byte-by-byte, character and line.

---
+++ java-io/src/main/java/java/streams/demos/App.java
@@ -0,0 +1,20 @@
+package java.streams.demos;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	
+    	OutputStream out = null;
+    	PrintStream ps = new PrintStream(out);
+    
+    	
+    
+    }
+}
