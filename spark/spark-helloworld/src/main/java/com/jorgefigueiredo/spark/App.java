92bdc40b737eb42c3f391a8aa2def6f3401d8e72
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2015-08-16 16:33:36 +0100

    Added two more examples

---
+++ spark/spark-helloworld/src/main/java/com/jorgefigueiredo/spark/App.java
@@ -0,0 +1,11 @@
+package com.jorgefigueiredo.spark;
+
+import static spark.Spark.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	get("/hello", (req, res) -> "Hello World");
+    }
+}
